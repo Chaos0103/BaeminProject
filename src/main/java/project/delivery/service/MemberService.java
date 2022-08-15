a9ef1882e1fe7f0999b6e3bf61ebc3deb228eca9
chaos0103 <lyt1228@naver.com>
2022-08-15 20:05:23 +0900

    feat: 회원정보수정기능
    - 닉네임, 비밀번호, 연락처, 주소 변경기능 구현

--- src/main/java/project/delivery/service/MemberService.java
+++ src/main/java/project/delivery/service/MemberService.java
@@ -1,6 +1,7 @@
 package project.delivery.service;
 
 import org.springframework.transaction.annotation.Transactional;
+import project.delivery.dto.AddressDto;
 import project.delivery.dto.MemberDto;
 
 @Transactional(readOnly = true)
@@ -13,7 +14,31 @@ public interface MemberService {
     @Transactional
     Long joinMember(MemberDto memberDto);
 
-    //회원수정
+    /**
+     * 회원정보수정: 닉네임 변경
+     */
+    @Transactional
+    void changeNickname(Long memberId, String nickname);
+
+    /**
+     * 회원정보수정: 비밀번호 변경
+     */
+    @Transactional
+    void changePassword(Long memberId, String password);
+
+    /**
+     * 회원정보수정: 연락처 변경
+     */
+    @Transactional
+    void changePhone(Long memberId, String phone);
+
+    /**
+     * 회원정보수정: 주소 변경
+     */
+    @Transactional
+    void changeAddress(Long memberId, AddressDto addressDto);
+
+
     //회원조회
     //회원삭제
 
