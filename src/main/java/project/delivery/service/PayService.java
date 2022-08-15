49ed446fe4ecddfca77a5dbe751c26bec97e7d74
chaos0103 <lyt1228@naver.com>
2022-08-16 01:27:15 +0900

    refactor: 배민페이내역
    - 배민페이사용, 배민페이충전, 배민페이환불 코드 통합

--- src/main/java/project/delivery/service/PayService.java
+++ src/main/java/project/delivery/service/PayService.java
@@ -1,17 +1,15 @@
 package project.delivery.service;
 
 import org.springframework.transaction.annotation.Transactional;
+import project.delivery.domain.TransactionType;
 
 @Transactional(readOnly = true)
 public interface PayService {
 
     /**
-     * 페이머니충전:
+     * 페이내역등록
      * @return 페이내역고유번호
      */
     @Transactional
-    Long chargePayMoney(Long memberId, int price);
-
-    @Transactional
-    Long consumePayMoney(Long memberId, int price, String content);
+    Long createPayHistory(Long memberId, int price, String content, TransactionType type);
 }
