9a128358ca0b5d3cf8868c65090fae0a2bf9aa04
chaos0103 <lyt1228@naver.com>
2022-08-16 23:07:17 +0900

    feat: 로그인서비스 구현
    - 로그인, 아이디찾기, 비밀번호찾기 구현
    - 로그인 권한 인증 설정

--- src/main/java/project/delivery/repository/MemberRepository.java
+++ src/main/java/project/delivery/repository/MemberRepository.java
@@ -12,4 +12,10 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByPhone(String phone);
 
     Optional<Member> findByNickname(String nickname);
+
+    Optional<Member> findByEmailAndPassword(String email, String password);
+
+    Optional<Member> findByUsernameAndPhone(String username, String phone);
+
+    Optional<Member> findByUsernameAndEmail(String username, String email);
 }
