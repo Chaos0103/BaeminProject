3f290da5271d68fd39e6cd6ad16c5118ad5726c1
chaos0103 <lyt1228@naver.com>
2022-09-09 16:26:52 +0900

    perf: 배민포인트 페이지 최적화

--- src/main/java/project/delivery/controller/PointController.java
+++ src/main/java/project/delivery/controller/PointController.java
@@ -12,70 +12,45 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import project.delivery.controller.form.VoucherSaveForm;
 import project.delivery.domain.*;
-import project.delivery.dto.NotificationDto;
-import project.delivery.dto.PointDto;
-import project.delivery.dto.PointHistoryDto;
-import project.delivery.dto.PointHistorySearch;
+import project.delivery.dto.*;
 import project.delivery.exception.DuplicateException;
 import project.delivery.exception.NoSuchException;
 import project.delivery.login.Login;
-import project.delivery.service.CouponService;
-import project.delivery.service.NotificationService;
-import project.delivery.service.PayService;
-import project.delivery.service.PointService;
+import project.delivery.service.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/points")
+@RequestMapping("/members/points")
 public class PointController {
 
     private final PointService pointService;
     private final PayService payService;
     private final CouponService couponService;
     private final NotificationService notificationService;
-
-    @ModelAttribute("loginMember")
-    public Member loginMember(@Login Member loginMember) {
-        return loginMember;
-    }
-
-    @ModelAttribute("pointTypes")
-    public PointType[] pointTypes() {
-        return PointType.values();
-    }
-
-    @ModelAttribute("voucherSaveForm")
-    public VoucherSaveForm voucherSaveForm() {
-        return new VoucherSaveForm();
-    }
+    private final BasketService basketService;
 
     /**
-     * @URL: localhost:8080/points
+     * @URL: localhost:8080/members/points
      */
     @GetMapping
     public String pointHome(
             @ModelAttribute("search") PointHistorySearch search,
             @Login Member loginMember, Model model) {
+        headerInfo(loginMember, model);
+        topInfo(loginMember, model);
 
-        //알림 조회
-        List<NotificationDto> notifications = notificationService.findNotificationByMemberId(loginMember.getId());
         //포인트 데이터 조회
         PointDto point = pointService.findPointByMemberId(loginMember.getId());
         //포인트 내역 조회
         List<PointHistoryDto> pointHistories = pointService.findPointHistoryByPointId(point.getId(), search);
-        //페이머니 잔액 조회
-        Integer money = payService.findMoney(loginMember.getId());
-        //사용 가능한 쿠폰 갯수 조회
-        Integer countCoupon = couponService.countCouponByMemberId(loginMember.getId());
 
-        model.addAttribute("notifications", notifications);
         model.addAttribute("point", point);
         model.addAttribute("pointHistories", pointHistories);
-        model.addAttribute("money", money);
-        model.addAttribute("countCoupon", countCoupon);
 
         model.addAttribute("voucherError", false);
         return "member/point";
@@ -105,5 +80,49 @@ public class PointController {
             model.addAttribute("pointHistories", pointHistories);
             return "member/point";
         }
+    }
+
+    @ModelAttribute("loginMember")
+    public Member loginMember(@Login Member loginMember) {
+        return loginMember;
+    }
+
+    @ModelAttribute("pointTypes")
+    public PointType[] pointTypes() {
+        return PointType.values();
+    }
+
+    @ModelAttribute("voucherSaveForm")
+    public VoucherSaveForm voucherSaveForm() {
+        return new VoucherSaveForm();
+    }
+
+    private void headerInfo(Member loginMember, Model model) {
+        //알림 조회
+        List<NotificationDto> notifications = notificationService.findNotificationByMemberId(loginMember.getId());
+        //장바구니 조회
+        List<BasketMenuDto> basketMenus = basketService.findAllByMemberId(loginMember.getId());
+        BasketDto basket = basketService.findBasketDto(loginMember.getId());
+
+        model.addAttribute("notifications", notifications);
+        model.addAttribute("basketMenus", basketMenus);
+        model.addAttribute("basket", basket);
+    }
+
+    private void topInfo(Member loginMember, Model model) {
+        Map<String, Object> topInfoMap = new HashMap<>();
+        //페이머니 잔액 조회
+        Integer payMoney = payService.findMoney(loginMember.getId());
+        //사용 가능한 쿠폰 갯수 조회
+        Integer countCoupon = couponService.countCouponByMemberId(loginMember.getId());
+        //포인트 잔액 조회
+        Integer totalPoint = pointService.findTotalPoint(loginMember.getId());
+
+        topInfoMap.put("grade", loginMember.getGrade().getDescription());
+        topInfoMap.put("payMoney", payMoney);
+        topInfoMap.put("countCoupon", countCoupon);
+        topInfoMap.put("totalPoint", totalPoint);
+
+        model.addAttribute("topInfoMap", topInfoMap);
     }
 }
