5a782d308cf8e83d9ac93d69e08fd61e617e94d3
chaos0103 <lyt1228@naver.com>
2022-09-17 01:25:21 +0900

    refactor: HomeController 코드 수정

--- src/main/java/project/delivery/controller/HomeController.java
+++ src/main/java/project/delivery/controller/HomeController.java
@@ -6,22 +6,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import project.delivery.domain.member.Member;
-import project.delivery.dto.BasketDto;
-import project.delivery.dto.NotificationDto;
 import project.delivery.login.Login;
-import project.delivery.service.query.NotificationQueryService;
-import project.delivery.service.query.BasketQueryService;
-
-import java.util.List;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
 
-    private final NotificationQueryService notificationQueryService;
-    private final BasketQueryService basketQueryService;
+    private final GlobalInformation globalInfo;
 
+    /**
+     * @URL: localhost:8080
+     */
     @GetMapping
     public String mainHome(@Login Member loginMember, Model model) {
 
@@ -29,11 +25,7 @@ public class HomeController {
             return "home";
         }
 
-        List<NotificationDto> notifications = notificationQueryService.findNotifications(loginMember.getId());
-        BasketDto basket = basketQueryService.findBasket(loginMember.getId());
-
-        model.addAttribute("notifications", notifications);
-        model.addAttribute("basket", basket);
+        globalInfo.headerInfo(loginMember, model);
         return "loginHome";
     }
 }
